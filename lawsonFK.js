d744f693b84908cc03dfada674c8c8b64e7725c6
hkhy4thw22 <100846912+hkhy4thw22@users.noreply.github.com>
2022-04-21 00:33:38 +0900

    2nd

--- lawsonFK.js
+++ lawsonFK.js
@@ -1,6 +1,42 @@
+const dbName = "campaignDatas";
 
+
+const openReq = indexedDB.open(dbName);
+openReq.onupgradeneeded = function(event){
+	myLog('---upgrage処理---');
+	const db = event.target.result;
+	const store_merchandise = db.createObjectStore("merchandise", {keyPath: 'no'});
+}
+openReq.onsuccess = function(event){
+
+}
+
+
+
+
+if(document.readyState !== "loading") {
+	main();
+	} else {
+	document.addEventListener("DOMContentLoaded", main, false);
+}
+
+
+function main(){
+	myLog("testtest");
+	
+	const searchTextBox = document.getElementById("searchTextBox");
+	const kekkaTable = document.getElementById("kekka");
+
+	searchTextBox.addEventListener("input",function(){
+		myLog("inputやで");
+
+	});
+
+}
 
 
 function myLog(str){
-    document.getElementById("myLogArea").innerHTML += "<br>"+str;
+	document.getElementById("myLogArea").innerHTML += "<br>"+str;
 }
+
+
